0ac7ba88bf0df001d694a78878d19e209b0f4f12
Adham Elarabawy <adhamelarabawy@gmail.com>
2020-05-16 06:15:53 -0700

    Finished translational engine

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -6,53 +6,78 @@ from mpl_toolkits.mplot3d import Axes3D
 from pytictoc import TicToc
 
 t = TicToc()
-
-
-class InverseKinematics:
-
-    def __init__(self, forearm, shoulder, hip_offset=(0, 0)):
-        self.wrist = forearm
-        self.shoulder = shoulder
-        self.hip_offset = hip_offset  # z, y
-
-    def from_local_xyz(self, side, x, y, z):
-        h1 = math.sqrt(self.hip_offset[0]**2 + self.hip_offset[1]**2)
-        h2 = math.sqrt(z**2 + y**2)
-        alpha_0 = math.atan(y / z)
-        alpha_1 = math.atan(self.hip_offset[1] / self.hip_offset[0])
-        alpha_2 = math.atan(self.hip_offset[0] / self.hip_offset[1])
-        alpha_3 = math.asin(h1 * math.sin(alpha_2 + math.radians(90)) / h2)
-        alpha_4 = math.radians(180) - (alpha_3 + alpha_2 + math.radians(90))
-        alpha_5 = alpha_1 - alpha_4
-        theta_h = alpha_0 - alpha_5
-
-        r0 = h1 * math.sin(alpha_4) / math.sin(alpha_3)
-        h = math.sqrt(r0**2 + x**2)
-        phi = math.asin(x / h)
-        theta_s = math.acos(
-            (h**2 + self.shoulder**2 - self.wrist**2) / (2 * h * self.shoulder)) - phi
-        theta_w = math.acos((self.wrist**2 + self.shoulder **
-                             2 - h**2) / (2 * self.wrist * self.shoulder))
-
-        if side == 'r':
-            return theta_h, theta_s, theta_w
-        if side == 'l':
-            return -theta_h, theta_s, theta_w
-        else:
-            raise ValueError(side)
+DEBUG = False
 
 
 class Leg:
     def __init__(self, origin):
         self.origin = origin
-        self.origin_x = origin[0]
-        self.origin_y = origin[1]
-        self.origin_z = origin[2]
+        self.hip_rad = None
+        self.shoulder_rad = None
+        self.wrist_rad = None
+        self.x = None
+        self.y = None
+        self.z = None
+
+    def __str__(self):
+        return f'origin: {self.origin}, angles: [hip: {math.degrees(self.hip_rad)}, shoulder: {math.degrees(self.shoulder_rad)}, wrist: {math.degrees(self.wrist_rad)}], endpoint: {(self.x, self.y, self.z)}'
+
+
+class InverseKinematics:
+
+    def __init__(self, forearm, shoulder, body_dim, hip_offset):
+        '''
+        body_dim: (length, width,thickness) in mm
+        '''
+        self.wrist = forearm
+        self.shoulder = shoulder
+        self.body_dim = body_dim
+        self.hip_offset = hip_offset  # z, y
+
+    def global_translation_engine(self, legs_xyz):
+        '''
+        Translation engine for global frame of reference (origin @ CoM of quadruped)
+        '''
+        pass
+
+    def global_rotation_engine(self, yaw, pitch, roll):
+        # roll calculations
+        t_0 = math.sin(pitch) * math.sin(pitch) / 2
+        # base_height =
+        pass
+
+    def local_translation_engine(self, legs_xyz):
+        joint_angles = []
+        for i, (x, y, z) in enumerate(legs_xyz):
+            h1 = math.sqrt(self.hip_offset[0]**2 + self.hip_offset[1]**2)
+            h2 = math.sqrt(z**2 + y**2)
+            alpha_0 = math.atan(y / z)
+            alpha_1 = math.atan(self.hip_offset[1] / self.hip_offset[0])
+            alpha_2 = math.atan(self.hip_offset[0] / self.hip_offset[1])
+            alpha_3 = math.asin(h1 * math.sin(alpha_2 + math.radians(90)) / h2)
+            alpha_4 = math.radians(
+                180) - (alpha_3 + alpha_2 + math.radians(90))
+            alpha_5 = alpha_1 - alpha_4
+            theta_h = alpha_0 - alpha_5
+
+            r0 = h1 * math.sin(alpha_4) / math.sin(alpha_3)
+            h = math.sqrt(r0**2 + x**2)
+            phi = math.asin(x / h)
+            theta_s = math.acos(
+                (h**2 + self.shoulder**2 - self.wrist**2) / (2 * h * self.shoulder)) - phi
+            theta_w = math.acos((self.wrist**2 + self.shoulder **
+                                 2 - h**2) / (2 * self.wrist * self.shoulder))
+
+            if i < 2:
+                joint_angles.append((theta_h, theta_s, theta_w))
+            else:
+                joint_angles.append((-theta_h, theta_s, theta_w))
+        return joint_angles
 
 
 class Quadruped:
 
-    def __init__(self, ax, body_dim=(300, 150), limb_lengths=(107, 115), offsets=(10, 60)):
+    def __init__(self, ax, origin=(0, 0, 100), body_dim=(300, 150), limb_lengths=(107, 115), offsets=(10, 60)):
         '''
         body_dim: (length, width,thickness) in mm
         limb_lengths: (upper_arm, bottom_arm) in mm
@@ -62,21 +87,29 @@ class Quadruped:
         self.body_dim = body_dim
         self.limb_lengths = limb_lengths
         self.offsets = offsets
+        self.origin = origin
 
         self.ik = InverseKinematics(
-            limb_lengths[0], limb_lengths[1], hip_offset=self.offsets)
+            limb_lengths[0], limb_lengths[1], self.body_dim, self.offsets)
 
-        self.body = [(-self.body_dim[0] / 2, -self.body_dim[1] / 2, 0),
-                     (self.body_dim[0] / 2, -self.body_dim[1] / 2, 0),
-                     (self.body_dim[0] / 2, self.body_dim[1] / 2, 0),
-                     (-self.body_dim[0] / 2, self.body_dim[1] / 2, 0),
-                     (-self.body_dim[0] / 2, -self.body_dim[1] / 2, 0)]
+        self.body = [(origin[0] - self.body_dim[0] / 2,
+                      origin[1] - self.body_dim[1] / 2, origin[2]),
+                     (origin[0] + self.body_dim[0] / 2,
+                      origin[1] - self.body_dim[1] / 2, origin[2]),
+                     (origin[0] + self.body_dim[0] / 2,
+                      origin[1] + self.body_dim[1] / 2, origin[2]),
+                     (origin[0] - self.body_dim[0] / 2,
+                      origin[1] + self.body_dim[1] / 2, origin[2]),
+                     (origin[0] - self.body_dim[0] / 2,
+                      origin[1] - self.body_dim[1] / 2, origin[2])]
 
         # back_right_leg, front_right_leg, front_left_leg, back_left_leg
-        self.legs = [Leg((-self.body_dim[0] / 2, -self.body_dim[1] / 2, 0)),
-                     Leg((self.body_dim[0] / 2, -self.body_dim[1] / 2, 0)),
-                     Leg((self.body_dim[0] / 2, self.body_dim[1] / 2, 0)),
-                     Leg((-self.body_dim[0] / 2, self.body_dim[1] / 2, 0))]
+        self.legs = [Leg((-self.body_dim[0] / 2, -self.body_dim[1] / 2, origin[2])),
+                     Leg((self.body_dim[0] / 2, -
+                          self.body_dim[1] / 2, origin[2])),
+                     Leg((self.body_dim[0] / 2,
+                          self.body_dim[1] / 2, origin[2])),
+                     Leg((-self.body_dim[0] / 2, self.body_dim[1] / 2, origin[2]))]
 
     @staticmethod
     def add_vector(base_vector, increment):
@@ -100,7 +133,10 @@ class Quadruped:
         Return the rotation matrix associated with counterclockwise rotation about
         the given axis by theta radians.
         """
-        print(f'vector: {vector}, axis: {axis}, theta: {math.degrees(theta)}')
+        global DEBUG
+        if DEBUG:
+            print(
+                f'vector: {vector}, axis: {axis}, theta: {math.degrees(theta)}')
         axis = np.asarray(axis)
         axis = axis / math.sqrt(np.dot(axis, axis))
         a = math.cos(theta / 2.0)
@@ -119,25 +155,20 @@ class Quadruped:
         z_data = [vector[2] for vector in self.body]
         self.ax.plot(x_data, y_data, z_data, color=color)
 
-    def draw_legs(self, desired_points=[(0, 100, 150), (0, 100, 150), (0, -100, 150), (00, -100, 150)], color='blue'):
+    def draw_legs(self, color='blue'):
         for i, leg in enumerate(self.legs):
-            desired_point = desired_points[i]
             leg_vectors = []
-            t.tic()
+
+            # IMPORTANT
+            # changing frame of reference for drawing vectors
+            wrist_rad = np.pi + leg.wrist_rad
+            shoulder_rad = leg.shoulder_rad
+            hip_rad = -leg.hip_rad
+
             if i < 2:
-                hip_rad, shoulder_rad, wrist_rad = self.ik.from_local_xyz(
-                    'r', desired_point[0], desired_point[1], desired_point[2])
                 horiz_offset = -self.offsets[1]
             else:
-                hip_rad, shoulder_rad, wrist_rad = self.ik.from_local_xyz(
-                    'l', desired_point[0], -desired_point[1], desired_point[2])
                 horiz_offset = self.offsets[1]
-            t.toc()
-
-            # changing frame of reference for drawing vectors
-            wrist_rad = np.pi + wrist_rad
-            shoulder_rad = shoulder_rad
-            hip_rad = -hip_rad
 
             hip_axis = (1, 0, 0)
 
@@ -188,20 +219,84 @@ class Quadruped:
             z_data = [vector[2] for vector in leg_vectors]
             self.ax.plot(x_data, y_data, z_data, color=color)
 
+    def fully_define(self, leg_points):
+        for i, leg in enumerate(self.legs):
+            leg.x = leg_points[i][0]
+            leg.y = leg_points[i][1]
+            leg.z = leg_points[i][2]
+
+        leg_angle_sets = self.ik.local_translation_engine(leg_points)
+
+        for i, leg_angle_set in enumerate(leg_angle_sets):
+            self.legs[i].hip_rad = leg_angle_set[0]
+            self.legs[i].shoulder_rad = leg_angle_set[1]
+            self.legs[i].wrist_rad = leg_angle_set[2]
+
+    def start_position(self):
+        starting_points = [(-50, 50, self.origin[2]),
+                           (-50, 50, self.origin[2]),
+                           (-50, 50, self.origin[2]),
+                           (-50, 50, self.origin[2])]
+        self.fully_define(starting_points)
+        for leg in self.legs:
+            print(str(leg))
+
+    def get_points_from_buffer(self):
+        return [(leg.x, leg.y, leg.z) for leg in self.legs]
+
+    def shift_body_xyz(self, x, y, z):
+        local_x_shift = x
+        local_y_shift = y
+        local_z_shift = z
+        shifts = (local_x_shift, local_y_shift, local_z_shift)
+        for i, leg in enumerate(self.legs):
+            leg.x += -local_x_shift
+            if i < 2:
+                leg.y += local_y_shift
+            else:
+                leg.y += -local_y_shift
+            leg.z += local_z_shift
+        self.fully_define(self.get_points_from_buffer())
+        self.origin = (self.origin[0] + local_x_shift,
+                       self.origin[1] + local_y_shift,
+                       self.origin[2] + local_z_shift)
+
+        self.body = [(self.origin[0] - self.body_dim[0] / 2, self.origin[1] - self.body_dim[1] / 2, self.origin[2]),
+                     (self.origin[0] + self.body_dim[0] / 2,
+                      self.origin[1] - self.body_dim[1] / 2, self.origin[2]),
+                     (self.origin[0] + self.body_dim[0] / 2,
+                      self.origin[1] + self.body_dim[1] / 2, self.origin[2]),
+                     (self.origin[0] - self.body_dim[0] / 2,
+                      self.origin[1] + self.body_dim[1] / 2, self.origin[2]),
+                     (self.origin[0] - self.body_dim[0] / 2, self.origin[1] - self.body_dim[1] / 2, self.origin[2])]
+        for leg in self.legs:
+            leg.origin = [leg.origin[i] + shift for i,
+                          shift in enumerate(shifts)]
+            print(leg.origin)
+
+    def shift_body_rotation(self, yaw, pitch, roll):
+        pass
+
 
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_aspect("equal")
-robot = Quadruped(ax)
-robot.draw_body()
-robot.draw_legs()
 
-WINDOW_SIZE = 300
-ax.set_xlim3d(-WINDOW_SIZE, WINDOW_SIZE)
-ax.set_ylim3d(-WINDOW_SIZE, WINDOW_SIZE)
-ax.set_zlim3d(-WINDOW_SIZE, WINDOW_SIZE)
+WINDOW_SIZE = 500
+ax.set_xlim3d(-WINDOW_SIZE / 2, WINDOW_SIZE / 2)
+ax.set_ylim3d(-WINDOW_SIZE / 2, WINDOW_SIZE / 2)
+ax.set_zlim3d(0, WINDOW_SIZE)
 
 ax.set_xlabel('x (mm)')
 ax.set_ylabel('y (mm)')
 ax.set_zlabel('z (mm)')
+robot = Quadruped(ax, origin=(0, 0, 170))
+robot.start_position()
+robot.shift_body_xyz(50, -50, 0)
+# robot.shift_body_rotation()
+
+robot.draw_body()
+robot.draw_legs()
+
+
 plt.show()
