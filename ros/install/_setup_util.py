0086106ec3ddad6f6365d4b3be76b7d3a207f27a
adham-elarabawy <adhamelarabawy@gmail.com>
2020-08-13 01:25:26 -0700

    remade

--- ros/install/_setup_util.py
+++ ros/install/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = '/home/aelarabawy/OpenQuadruped/ros/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- ros/install/share/open_quadruped/cmake/open_quadrupedConfig.cmake
+++ ros/install/share/open_quadruped/cmake/open_quadrupedConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/aelarabawy/OpenQuadruped/ros/install/lib;/opt/ros/melodic/lib)
+    foreach(path /home/aelarabawy/OpenQuadruped/ros/install/lib;/home/aelarabawy/OpenQuadruped/ros/devel/lib;/opt/ros/melodic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
