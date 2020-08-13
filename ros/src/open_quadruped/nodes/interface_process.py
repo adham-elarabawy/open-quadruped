54578478a4d5a9d25d2165361fa3c320e7a2d712
adham-elarabawy <adhamelarabawy@gmail.com>
2020-08-13 00:30:32 -0700

    IK lib implementation

--- ros/src/open_quadruped/nodes/interface_process.py
+++ ros/src/open_quadruped/nodes/interface_process.py
@@ -6,33 +6,50 @@ from std_msgs.msg import String, Float32, Int32
 from sensor_msgs.msg import Joy 
 from open_quadruped.msg import JointAngles
 
+from lib import leg_ik, body_ik
+
 body_mode = 0
 gait_mode = 1
 mode=body_mode
+
+leg_model = leg_ik.LegIKModel(109.868, 144.580, 11.369, 63.763)
+body_model = body_ik.BodyIKModel(76.655, 229.3, 130)
+
+buttons = None
+axes = None
  
-#define function/functions to provide the required functionality
-def fnc_callback(msg):
-    global mode
+# Controller callback
+def controller_callback(msg):
+    global mode, body_mode, gait_mode, buttons, axes
     if msg.buttons[7] == 1:
         mode = gait_mode
     else:
         mode = body_mode
-    rospy.loginfo(str(mode))
+    buttons = msg.buttons
+    axes = msg.axes
+#    rospy.loginfo(str(mode))
  
 if __name__=='__main__':
     #Add here the name of the ROS. In ROS, names are unique named.
     rospy.init_node('interface_process', log_level=rospy.DEBUG)
     #subscribe to a topic using rospy.Subscriber class
-    sub=rospy.Subscriber('joy', Joy, fnc_callback)
-    rospy.spin()
+    sub=rospy.Subscriber('joy', Joy, controller_callback)
     #publish messages to a topic using rospy.Publisher class
-    pub=rospy.Publisher('joint_angles', JointAngles, queue_size=1)
-    rate=rospy.Rate(300) # publish rate: 300 hz
+    pub=rospy.Publisher('joint_angles', JointAngles, queue_size=10)
+    rate=rospy.Rate(10) # publish rate: 300 hz
  
     while not rospy.is_shutdown():
         if mode == body_mode:
-            test
-        else if mode == gait_mode:
+            val = 'in body mode'
+        elif mode == gait_mode:
+            val = 'in gait mode'
+        rospy.loginfo(val)
+
+        ja = JointAngles()
+        ja.fl = [0,0,0]
+        ja.fr = [1,1,1]
+        ja.bl = [2,2,2]
+        ja.br = [3,3,3]
  
-        pub.publish(varP)
+        pub.publish(ja)
         rate.sleep()
