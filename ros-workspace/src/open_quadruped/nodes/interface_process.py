4e6983679ea00c6147e456d01fa462ebd6e24764
adham-elarabawy <adhamelarabawy@gmail.com>
2020-12-27 17:43:06 -0800

    Fixed formatting

--- ros-workspace/src/open_quadruped/nodes/interface_process.py
+++ ros-workspace/src/open_quadruped/nodes/interface_process.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
- 
+
 import rospy, time, math, cv2, sys, math
- 
+
 from std_msgs.msg import String, Float32, Int32
-from sensor_msgs.msg import Joy 
+from sensor_msgs.msg import Joy
 from open_quadruped.msg import JointAngles
 
 from control_library import leg_ik, body_ik
@@ -25,26 +25,26 @@ body_model = body_ik.BodyIKModel(76.655, 229.3, 130)
 # setup joystick containers
 buttons = None
 axes = None
- 
+
 # Controller callback
 def controller_callback(msg):
     global mode, body_mode, gait_mode, buttons, axes
     if msg.buttons[7] == 1:
-        mode = gait_mode 
+        mode = gait_mode
     else:
         mode = body_mode
 
     buttons = msg.buttons
     axes = msg.axes
- 
+
 if __name__=='__main__':
 
-    # setup ROS node + pub/sub 
+    # setup ROS node + pub/sub
     rospy.init_node('interface_process', log_level=rospy.DEBUG)
     sub=rospy.Subscriber('joy', Joy, controller_callback)
     pub=rospy.Publisher('joint_angles', JointAngles, queue_size=10)
     rate=rospy.Rate(10) # publish rate: 10 hz
- 
+
     while not rospy.is_shutdown():
         if not (axes is None or buttons is None):
             if mode == body_mode:
@@ -73,7 +73,7 @@ if __name__=='__main__':
             ja.fr = ja_m[1]
             ja.bl = ja_m[2]
             ja.br = ja_m[3]
-     
+
             #publishing JointAngles message to topic
             pub.publish(ja)
             rate.sleep()
